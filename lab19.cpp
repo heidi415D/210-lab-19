3ed9bd984dac60c2313fda925b091a9f6df55805
1080heidi <heidideleon415@gmail.com>
2025-10-05 20:06:56 -0700

    Creating class

--- lab19.cpp
+++ lab19.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cstdlib> // rand, srand
+#include <ctime>   // time
+using namespace std;
+
+// each review node in the linked list
+struct ReviewNode{
+    double rating; // random rating between 1.0 and 5.0
+    string comment; // full comment text
+    ReviewNode* next; // pointer to the next review node
+};
+
+// movie class that hold a title and its linked list of reviews
+class Movie{
+private:
+    string title; // movie title
+    ReviewNode* head; // start of linked list
+
+public:
+    Movie(string t = ""){
+        title = t;
+        head = nullptr;
+    }
+}
